9a86427a2cdb313fb53abcfcfbd8a86abf5705ad
ClementeVC <47197274+ClementeVC@users.noreply.github.com>
2021-03-12 15:15:13 -0600

    Add files via upload

---
+++ precache-manifest.db81dce6ed799a11021e99067b9af8f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec96edba6a8ae42f9b7bbb9a689d4ab4",
+    "url": "/calderon_practica2/index.html"
+  },
+  {
+    "revision": "caba6f0d8479cbbb4c78",
+    "url": "/calderon_practica2/static/css/main.c6c3791b.chunk.css"
+  },
+  {
+    "revision": "92adfd0cfc0e924807e2",
+    "url": "/calderon_practica2/static/js/2.f38ce821.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/calderon_practica2/static/js/2.f38ce821.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "caba6f0d8479cbbb4c78",
+    "url": "/calderon_practica2/static/js/main.6aadac6e.chunk.js"
+  },
+  {
+    "revision": "070f097cf16a98900e50",
+    "url": "/calderon_practica2/static/js/runtime-main.86c27667.js"
+  }
+]);
